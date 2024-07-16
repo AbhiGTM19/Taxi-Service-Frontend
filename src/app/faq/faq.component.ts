cb7efe1358ccc6d6fd53a2c1d02c3beb639c927e
Abhishek Gautam <137047583+AbhiGTM19@users.noreply.github.com>
2024-07-16 19:28:47 +0530

    All three components are unstable and are not finalized.

--- src/app/faq/faq.component.ts
+++ src/app/faq/faq.component.ts
@@ -1,6 +1,18 @@
+import { HttpClientModule } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatExpansionModule } from '@angular/material/expansion';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AppRoutingModule } from '../app-routing.module';
 
 @Component({
   selector: 'app-faq',
