747b0f1cb4f992f2fa5a7078f8836cf2798206e8
Abhishek Gautam <137047583+AbhiGTM19@users.noreply.github.com>
2024-07-28 19:27:26 +0530

    Changes in UI component of landing page.

--- src/app/figma-test/figma-test.component.ts
+++ src/app/figma-test/figma-test.component.ts
@@ -17,6 +17,10 @@ export class FigmaDesignPageComponent implements OnInit {
     window.location.href = 'https://forms.gle/wagu22L1PyL1NySP6';
   }
 
+  feedback(){
+    window.location.href = "https://www.google.com/maps/place/RV+Taxi+Services/@25.3760933,83.0185718,17z/data=!3m1!4b1!4m6!3m5!1s0x398e2f001ebd7a47:0x2f77133add222b87!8m2!3d25.3760885!4d83.0234427!16s%2Fg%2F11w5b0pl0_?entry=ttu"
+  }
+
   redirectToFacebook() {
     window.location.href = "https://www.facebook.com/profile.php?id=61562391380565"
   }
@@ -24,5 +28,7 @@ export class FigmaDesignPageComponent implements OnInit {
   redirectToInstagram() {
     window.location.href = "https://www.instagram.com/rvta.xi/"
   }
+
+
 
 }
