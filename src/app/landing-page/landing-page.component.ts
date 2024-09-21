cb6bc4fbe15dad61d77031caa30c73dec60b90b4
Abhishek Gautam <137047583+AbhiGTM19@users.noreply.github.com>
2024-09-21 18:57:45 +0530

    All changes are made for template.

--- src/app/landing-page/landing-page.component.ts
+++ src/app/landing-page/landing-page.component.ts
@@ -14,19 +14,19 @@ export class LandingPageComponent implements AfterViewInit {
     document.addEventListener('DOMContentLoaded', function () {
 
       ScrollReveal({
-        distance: '60px',
+        distance: '50px',
         duration: 1000,
         delay: 300,
         opacity: 0,
         easing: "ease-in-out",
       });
 
-      ScrollReveal().reveal('main .services-gif, main .amenities-gif', { origin: 'bottom', reset: true });
+      ScrollReveal().reveal('main .services-gif, main .amenities-gif', { origin: 'bottom', reset: false });
 
       ScrollReveal().reveal('main .man-with-phone-showing-thumbs-up, main .smiling-man-and-woman-with-phones')
-        , { origin: 'bottom', reset: true }
+        , { origin: 'bottom', reset: false }
 
-      ScrollReveal().reveal('main .heading, main .text', { origin: 'bottom', reset: true });
+      ScrollReveal().reveal('main .heading, main .text', { origin: 'bottom', reset: false });
 
       // ScrollReveal().reveal('main .h3', { origin: 'bottom', reset: true });
 
